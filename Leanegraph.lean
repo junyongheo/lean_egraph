a5982e24529c440857875754d1de87a169d330de
Junyong Heo <108453274+junyongheo@users.noreply.github.com>
2026-02-01 21:58:45 +0100

    Update Leanegraph.lean
    This should cover all the library impls

--- Leanegraph.lean
+++ Leanegraph.lean
@@ -1,3 +1,6 @@
 -- This module serves as the root of the `Leanegraph` library.
 -- Import modules here that should be built as part of the library.
 import Leanegraph.Basic
+import Leanegraph.core
+import Leanegraph.framework
+-- I think that's it?
